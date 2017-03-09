fe9bafb098faaa6377b277cca7a8a62837d1be16
Jamie Barron <gumbald@gmail.com>
2017-03-09 22:12:17 +0000

    Corrected a derp in access token

--- exercise.py
+++ exercise.py
@@ -241,7 +241,7 @@ print "Photo booth app running..."
 
 api = twitter.Api(consumer_key=config.consumer_key,
                       consumer_secret=config.consumer_secret,
-                      access_token_key=config.access_token,
+                      access_token_key=config.access_token_key,
                       access_token_secret=config.access_token_secret)
                       
 print(api.VerifyCredentials())
