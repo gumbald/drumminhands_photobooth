84af77e04882c9fb4132b02f7b6e86d19feee713
gumbald <gumbald@gmail.com>
2017-03-10 20:21:05 +0000

    Code cleanup, random timings between pose numbers

--- exercise.py
+++ exercise.py
@@ -20,6 +20,7 @@ from pygame.locals import QUIT, KEYDOWN, K_ESCAPE
 import config # this is the config python file config.py
 from signal import alarm, signal, SIGALRM, SIGKILL
 from time import gmtime, strftime, sleep
+from random import randint
 import definitions as r
 import twitter
 
@@ -31,23 +32,17 @@ btn_pin = 18 # pin for the start button
 
 total_pics = 4 # number of pics to be taken
 capture_delay = 1 # delay between pics
-prep_delay = 5 # number of seconds at step 1 as users prep to have photo taken
+prep_delay = 2 # number of seconds at step 1 as users prep to have photo taken
 gif_delay = 100 # How much time between frames in the animated gif
 restart_delay = 10 # how long to display finished message before beginning a new session
 test_server = 'www.google.com'
-
-# full frame of v1 camera is 2592x1944. Wide screen max is 2592,1555
-# if you run into resource issues, try smaller, like 1920x1152. 
-# or increase memory http://picamera.readthedocs.io/en/release-1.12/fov.html#hardware-limits
-high_res_w = 1296 # width of high res image, if taken
-high_res_h = 972 # height of high res image, if taken
 
 #############################
 ### Variables that Change ###
 #############################
 # Do not change these variables, as the code will change it anyway
 transform_x = config.monitor_w # how wide to scale the jpg when replaying
-transfrom_y = config.monitor_h # how high to scale the jpg when replaying
+transform_y = config.monitor_h # how high to scale the jpg when replaying
 offset_x = 0 # how far off to left corner to display photos
 offset_y = 0 # how far off to left corner to display photos
 replay_delay = 1 # how much to wait in-between showing pics on-screen after taking
@@ -168,7 +163,7 @@ def show_image(image_path):
 	set_dimensions(img.get_width(), img.get_height())
 
 	# rescale the image to fit the current display
-	img = pygame.transform.scale(img, (transform_x,transfrom_y))
+	img = pygame.transform.scale(img, (transform_x,transform_y))
 	screen.blit(img,(offset_x,offset_y))
 	pygame.display.flip()	
 	
@@ -195,6 +190,13 @@ def start_photobooth():
 	#GPIO.output(led_pin,False);
 	show_image(real_path + "/instructions.png")
 	sleep(prep_delay)
+	
+	show_image(real_path + "/pose3.png")
+	sleep(randint(1,3))
+	show_image(real_path + "/pose2.png")
+	sleep(randint(1,3))
+	show_image(real_path + "/pose1.png")
+	sleep(randint(1,3))
 	
 	# clear the screen
 	clear_screen()
